cda9aa8006ca0f77405b9953d2365147a9719623
Oscar <oscarponteuk@gmail.com>
2023-06-20 19:35:43 +0100

    Add Update method to TodoRepository for updating TodoItems.

--- src/Todo.Data/ITodoRepository.cs
+++ src/Todo.Data/ITodoRepository.cs
@@ -7,4 +7,11 @@ public interface ITodoRepository
     Task<IEnumerable<TodoItem>> List();
     
     Task<Guid> Create(TodoItem newItem);
+
+    /// <summary>
+    /// Updates a TodoItem in the database.
+    /// </summary>
+    /// <param name="updatedItem">The updated TodoItem object.</param>
+    /// <returns>A task representing the asynchronous operation with a boolean indicating the success of the update operation.</returns>
+    Task<bool> Update(TodoItem updatedItem);
 }
